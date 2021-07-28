8e54f01878e1f4453eacee41e409ba23a4d97ddd
Alexis <alex_91y@hotmail.com>
2021-07-28 11:05:09 -0500

    se hacen cambios en el worker y client

--- client.py
+++ client.py
@@ -35,13 +35,14 @@ except TypeError as e:
 # json_result = {}
 # json_result['response'] = lista_result_response
 json_result_text = json.dumps(json_job_result, indent=4)
+print(json_result_text)
 
-list_errors = JsonUtils.exist_errors_in_play_button_data(json_job_result, modo_debug)
-
-# verifica que al menos no haya algun error localizado en la lista de errores/validaciones de las vigencias y push
-# buttons, en caso contrario, se envia la notificacion por email
-if len(list_errors) > 0:
-    HTML = HtmlUtils.generate_html_table_errors_push_buttons(list_errors)
-    subject = const.SUBJECT_MAIL_INCONSISTENCIA_PLAY_BUTTON
-    resp = MailUtils.send_email(lista_correos_destinatarios, 'notificacion.itoc@triara.com', subject, HTML)
-    print(resp.text)
+# list_errors = JsonUtils.exist_errors_in_play_button_data(json_job_result, modo_debug)
+#
+# # verifica que al menos no haya algun error localizado en la lista de errores/validaciones de las vigencias y push
+# # buttons, en caso contrario, se envia la notificacion por email
+# if len(list_errors) > 0:
+#     HTML = HtmlUtils.generate_html_table_errors_push_buttons(list_errors)
+#     subject = const.SUBJECT_MAIL_INCONSISTENCIA_PLAY_BUTTON
+#     resp = MailUtils.send_email(lista_correos_destinatarios, 'notificacion.itoc@triara.com', subject, HTML)
+#     print(resp.text)
